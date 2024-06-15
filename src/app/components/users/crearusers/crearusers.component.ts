8649662aec631d47faa52c3b4955ced068589369
Joaquin Rivera <joaquinrivram@arizona.edu>
2024-06-15 13:48:25 -0500

    update create users

--- src/app/components/users/crearusers/crearusers.component.ts
+++ src/app/components/users/crearusers/crearusers.component.ts
@@ -7,19 +7,15 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Users } from '../../../models/users';
 import { Gender } from '../../../models/genders';
 import { Personalities } from '../../../models/personalities';
 import { UsersService } from '../../../services/users.service';
 import { PersonalitiesService } from '../../../services/personalities.service';
 import { GendersService } from '../../../services/genders.service';
+import { RolesService } from '../../../services/roles.service';
+import { Roles } from '../../../models/roles';
 
 @Component({
   selector: 'app-crearusers',
@@ -53,7 +49,8 @@ export class CrearusersComponent implements OnInit {
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private sP: PersonalitiesService,
-    private sG: GendersService
+    private sG: GendersService,
+    private sR: RolesService // Inyectar RolesService
   ) {}
 
   ngOnInit(): void {
@@ -90,42 +87,64 @@ export class CrearusersComponent implements OnInit {
 
   aceptar(): void {
     if (this.form.valid) {
-      this.u.idUser = this.form.value.codigo
-      this.u.username = this.form.value.nombre
-      this.u.emailUser = this.form.value.email
-      this.u.password = this.form.value.contrasenia
-      this.u.ageUser = this.form.value.edad
-      this.u.enabled = this.form.value.enabled
-      this.u.gender.idGender = this.form.value.genero
-      this.u.personality.idPersonality = this.form.value.personalidad
-      if(this.edicionusers){
-        this.sU.update(this.u).subscribe((data) => {
+      this.u.idUser = this.form.value.codigo;
+      this.u.username = this.form.value.nombre;
+      this.u.emailUser = this.form.value.email;
+      this.u.password = this.form.value.contrasenia;
+      this.u.ageUser = this.form.value.edad;
+      this.u.enabled = this.form.value.enabled;
+      this.u.gender.idGender = this.form.value.genero;
+      this.u.personality.idPersonality = this.form.value.personalidad;
+
+      if (this.edicionusers) {
+        this.sU.update(this.u).subscribe(() => {
           this.sU.list().subscribe((data) => {
             this.sU.setList(data);
           });
         });
-      }else{
-        this.sU.insert(this.u).subscribe((data) => {
-          this.sU.list().subscribe((data) => {
-            this.sU.setList(data);
-          });
+      } else {
+        this.sU.insert(this.u).subscribe((newUser: Users) => {
+          if (newUser && newUser.idUser) {
+            console.log('Usuario creado:', newUser);
+            this.createRoleForUser(newUser.idUser); // Crear rol para el usuario
+            this.sU.list().subscribe((data) => {
+              this.sU.setList(data);
+            });
+            this.router.navigate(['usuarios']);
+          } else {
+            console.error('Error: Usuario no creado correctamente');
+          }
+        }, error => {
+          console.error('Error al crear usuario:', error);
         });
       }
-    this.router.navigate(['usuarios']);
     }
   }
+
+  createRoleForUser(userId: number): void {
+    const newRole = new Roles();
+    newRole.rol = 'STUDENT'; // o cualquier rol por defecto que desees asignar
+    newRole.user = { idUser: userId } as Users;
+
+    this.sR.insert(newRole).subscribe((data) => {
+      this.sR.list().subscribe((data) => {
+        this.sR.setList(data);
+      });
+    });
+  }
+
   init() {
     if (this.edicionusers) {
       this.sU.listId(this.id).subscribe((data) => {
-        this.form = new FormGroup({
-          codigo: new FormControl(data.idUser),
-          nombre: new FormControl(data.username),
-          email: new FormControl(data.emailUser),
-          contrasenia: new FormControl(data.password),
-          edad: new FormControl(data.ageUser),
-          enabled: new FormControl(data.enabled),
-          genero: new FormControl(data.gender.idGender),
-          personalidad: new FormControl(data.personality.idPersonality),
+        this.form = this.formBuilder.group({
+          codigo: data.idUser,
+          nombre: data.username,
+          email: data.emailUser,
+          contrasenia: data.password,
+          edad: data.ageUser,
+          enabled: data.enabled,
+          genero: data.gender.idGender,
+          personalidad: data.personality.idPersonality,
         });
       });
     }
