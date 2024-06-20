71bb2734c0829696f3cb8ad31661d98f9eab9bc9
Joaquin Rivera <joaquinrivram@arizona.edu>
2024-06-20 11:21:46 -0500

    update front

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { LoginService } from '../../services/login.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    NgIf
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  constructor(private loginService: LoginService) {}
+
+  username: string = '';
+  role: string = '';
+
+  ngOnInit(): void {
+    this.username = this.loginService.showName();
+  }
+
+  verificar() {
+    this.role = this.loginService.showRole();
+    //this.usuario = this.loginService.userlogin(this.joaquin);
+    this.username = this.loginService.showName();
+
+    return this.loginService.verificar();
+  }
+
+  isADMIN() {
+    return this.role === 'ADMIN';
+  }
+
+  isCOACH() {
+    return this.role === 'COACH';
+  }
+
+  isSTUDENT() {
+    return this.role === 'STUDENT';
+  }
+  
 }
