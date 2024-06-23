2861846a365e789c12f49ba2956672936cce9de9
Kevin16042 <133721686+Kevin16042@users.noreply.github.com>
2024-06-22 23:46:08 -0500

    report3

---
+++ src/app/components/report/report3/report3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report3',
+  standalone: true,
+  imports: [],
+  templateUrl: './report3.component.html',
+  styleUrl: './report3.component.css'
+})
+export class Report3Component {
+
+}
