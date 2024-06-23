29966988452bca6fbb3b7e4cb450ae2dfc64283e
Kevin16042 <133721686+Kevin16042@users.noreply.github.com>
2024-06-22 23:49:28 -0500

    Update report.component.ts
    ts del report

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-report',
+  selector: 'app-reportes',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './report.component.html',
   styleUrl: './report.component.css'
 })
-export class ReportComponent {
-
+export class ReportesComponent implements OnInit{
+  constructor (public route: ActivatedRoute){}
+  ngOnInit(): void {
+      
+  }
 }
