a33b9de6c0b2fabcb6722d6af5c3659317277277
Albertofran <111243166+AlbertoBfran@users.noreply.github.com>
2024-06-22 23:19:42 -0500

    asdaw
    dawdaw

--- src/app/services/genders.service.ts
+++ src/app/services/genders.service.ts
@@ -4,35 +4,5 @@ import { Gender } from '../models/genders';
 import { enviroment } from '../../enviroments/enviroment';
 import { Subject } from 'rxjs';
 
-const base_url = enviroment.base
-@Injectable({
-  providedIn: 'root'
-})
-export class GendersService {
-  private url = `${base_url}/gender`
-  private urlinsert = `${base_url}/gender/Registro`
-  private listaCambio = new Subject<Gender[]>();
-  constructor(private http:HttpClient) { }
-  list(){
-    return this.http.get<Gender[]>(this.url)
-  }
-  insert(g: Gender) {
-    return this.http.post(this.urlinsert, g);
-  }
-  setList(listaNueva: Gender[]) {
-    this.listaCambio.next(listaNueva);
-  }
-  getList() {
-    return this.listaCambio.asObservable()
-  }
-  listId(id: number) {
-    return this.http.get<Gender>(`${this.url}/${id}`);
-  }
-  update(g: Gender, id:number) {
-    return this.http.put(`${this.url}/${id}`, g);
-  }
-  eliminar(id: number) {
-    return this.http.delete(`${this.url}/${id}`);
-  }
-}
+aaaa
  //gender class entity///
