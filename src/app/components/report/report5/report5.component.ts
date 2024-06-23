6ff05f79948385e133b9a674d31724818ee27441
Kevin16042 <133721686+Kevin16042@users.noreply.github.com>
2024-06-22 23:46:46 -0500

    report5

---
+++ src/app/components/report/report5/report5.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report5',
+  standalone: true,
+  imports: [],
+  templateUrl: './report5.component.html',
+  styleUrl: './report5.component.css'
+})
+export class Report5Component {
+
+}
